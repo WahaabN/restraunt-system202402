0a2719025147177ac7fc417681144ad85bab5246
WahaabN <wahaabn@gmail.com>
2024-02-25 05:38:34 +0000

    AdminAddOrder fix + chakra UI
    - Authenticated AdminAddOrder
    - Added chakra UI (order cards)

---
+++ restaurant-system/resources/js/Pages/AdminAddOrder.jsx
@@ -0,0 +1,71 @@
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import { Head } from '@inertiajs/react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+export default function AdminAddOrder(props) {
+
+  const { accessToken } = props;
+
+  function createOrder() {
+
+    console.log('/api/createOrder', { orderName });
+    
+    // Send a POST request to update the status
+    axios.post('/api/createOrder', { orderName },{
+      headers: {
+        'Authorization': `Bearer ${accessToken}`
+    }
+    })
+      .then(response => {
+        console.log('Status updated successfully:', response.data);
+      })
+      .catch(error => {
+        console.error('Error updating status:', error);
+      });
+    console.log('/api/createOrder', { orderName });
+    
+    // Send a POST request to update the status
+    axios.post('/api/createOrder', { orderName },{
+      headers: {
+        'Authorization': `Bearer ${accessToken}`
+    }
+    })
+      .then(response => {
+        console.log('Status updated successfully:', response.data);
+      })
+      .catch(error => {
+        console.error('Error updating status:', error);
+      });
+
+
+
+  }
+
+const [orderName, setOrderName] = useState('')
+
+
+const handleInputChange = (formItem) => {
+  setOrderName(formItem.target.value)
+
+}
+
+
+const submitOrder = ()=>{
+  createOrder();
+  setOrderName('')
+}
+ 
+    return (
+      <div className='container'>
+
+        
+          <h1>THIS IS THE ORDER SECTION</h1>
+
+     
+      <input value = {orderName} onChange = {handleInputChange} type="text"></input>
+      <button onClick={submitOrder}>submit</button>
+      
+      </div>    );
+}
